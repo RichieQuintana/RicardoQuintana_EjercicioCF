59bb7b7e83b88199d3375e9090cf42149e1d2e91
laboratorio <laboratorio@UPOAULA10617>
2023-10-11 08:20:54 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RicardoQuintana_EjercicioCF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
